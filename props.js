381e1a7de8bb9e4beaeffac3d5bd6d7bb5367f09
Erin Kahn Smith <erinnicolekahn@gmail.com>
2021-11-23 11:41:01 -0500

    Create props.js

---
+++ props.js
@@ -0,0 +1,7 @@
+Props - properties used to handle data, a way to pass existing data to a React component, 
+however the component cannot change the props - they're read-only, a one way data flow…
+so you need state to be able to access private data and change it
+Props are immutable/ can’t change inside the body of a component/function…you can only change the VALUE of a prop
+- These DON’T change *** immutable
+- Anytime you want to pass data into a component so that component can determine what will get displayed on the
+screen.
