28832b788c9b06e7a916062729ed795bef464f79
Erin Kahn Smith <erinnicolekahn@gmail.com>
2021-11-23 11:42:00 -0500

    Create create-react-app.js

---
+++ create-react-app.js
@@ -0,0 +1,37 @@
+Setup:
+Create empty folder
+Cd into it
+
+Run commands:
+npx create-react-app comment-app (files will be auto created after this command)
+cd comment-app
+npm start
+
+You should have a public folder and a src folder as well as other single files like package.json, node_modules and gitignore
+
+localhost:3000 Port will open in chrome
+
+
+Main top level entry points to the application:
+index.js and index.html
+
+React chrome developer tools
+- Components tab
+    - Good for debugging and seeing the whole app tree
+- Profiler tab 
+    - record interactions
+    - Good for checking performance, how long things take to load
+
+
+
+To run a production build: (minified files)
+npm run build
+
+You will see a build folder with files inside it
+
+sudo npm install -g serve 
+serve -s build
+
+http://localhost:5000  port will be what you paste in your browser
+
+React extension icon will be black now - production build 
