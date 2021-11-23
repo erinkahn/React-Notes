e70b0078bee60b39f00fe2fe5cdeb36201e491b1
Erin Kahn Smith <erinnicolekahn@gmail.com>
2021-11-23 11:40:44 -0500

    Create state.js

---
+++ state.js
@@ -0,0 +1,13 @@
+State -  any data that should be saved and modified without necessarily being added to a database - for example, adding and removing items from a shopping cart before confirming your purchase.
+- These change ** are mutable
+- A way for React to remember saved values from within a component.
+- This is similar to declaring variables from within a component
+
+Array restructuring (state)
+useState returns an ARRAY
+Which is why it’s written like: const [thing, setThing] = React.useState(“”)
+setThing is a function (second parameter is a function)
+
+
+A parent component can pass a state back to its children by using props. 
+This is what we call “Lifting state up” and then having a “top-down” data flow.
