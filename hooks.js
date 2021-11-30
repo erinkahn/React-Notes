85214f41a137b8516a37ed03eaa3d152953c19d9
Erin Kahn Smith <erinnicolekahn@gmail.com>
2021-11-30 18:52:36 -0500

    Update hooks.js

--- hooks.js
+++ hooks.js
@@ -1,6 +1,6 @@
-React Hooks
-- createContext - used to make a global change/function to multiple components in your app, such as the language selected, the theme (dark or light), chosen currency, user profile details
-- useRef -  used to reference some element/object in your code
-- useFetch -  used to get or post data from or to a database or API 
-- useEffect - lets you perform an effect in your component (setTimeout, setInterval - both require a cleanup function inside the useEffect call)
-- useState - used when the state/value is changing on an element or object
+// React Hooks
+  // - createContext - used to make a global change/function to multiple components in your app, such as the language selected, the theme (dark or light), chosen currency, user profile details
+  // - useRef -  used to reference some element/object in your code
+  // - useFetch -  used to get or post data from or to a database or API 
+  // - useEffect - lets you perform an effect in your component (setTimeout, setInterval - both require a cleanup function inside the useEffect call)
+  // - useState - used when the state/value is changing on an element or object
