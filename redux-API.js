eba90264fab5d1f5419226541a58ff3a4d2b1996
Erin Kahn Smith <erinnicolekahn@gmail.com>
2021-11-30 19:55:35 -0500

    Create redux-API.js

---
+++ redux-API.js
@@ -0,0 +1,10 @@
+// Redux API
+  // https://redux.js.org/introduction/core-concepts
+  // building Redux applications
+  // https://www.codecademy.com/learn/learn-redux/modules/core-redux-api/cheatsheet
+
+createStore()
+
+store.getState()
+store.dispatch(action)
+store.subscribe(listener)
