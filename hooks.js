02a253d6911cbec65dcb4169f374b3bd1231c575
Erin Kahn Smith <erinnicolekahn@gmail.com>
2021-12-20 11:15:54 -0500

    Update hooks.js

--- hooks.js
+++ hooks.js
@@ -2,12 +2,18 @@
   
   // createContext - used to make a global change/function to multiple components in your app, such as the language selected, the theme (dark or light), chosen currency, user profile details
   
-  // useContext - used to access the user context in all components
+  // useContext - manages GLOBAL state, used to access the user context globally in all components
+
+  // useReducer - manages COMPLEX state, used check if there are multiple state changes and what those changes are
+
+  // useState - manages SIMPLE state, used when the state/value is changing on an element or object
   
-  // useRef -  used to reference some element/object in your code since you can't do document.querySelector(element) in react
+  // useRef -  used to create a reference to something or set a variable reference to an element in your DOM
   
   // useFetch -  used to get or post data from or to a database or API 
   
   // useEffect - lets you perform an effect in your component after the component renders and the DOM has been updated ( examples: setTimeout, onScroll, change document.title, setInterval, fetch data, setup a subscription, change the DOM )
       
-  // useState - used when the state/value is changing on an element or object
+
+
+
