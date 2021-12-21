f167a01b2f515d940238ec4cee69ffb882c3e8e3
Erin Kahn Smith <erinnicolekahn@gmail.com>
2021-12-21 17:30:06 -0500

    Update react.lazy.js

--- react.lazy.js
+++ react.lazy.js
@@ -1,82 +1,82 @@
 // React.lazy + <Suspense/>
     // - If a component is a very large component (that loads several libraries) and is only used by a small portion of the users, it is wise to lazy load it (only load it when it's used) to improve performance
-
-// example:
-    // usage would be and Admin component (logged in) - bc some people don’t want to log in or are not admins at all
-
-// Step 1: lazy load
-
-    // Instead of :
-    Import Admin from ‘./Admin.js’;
-
-    // write :
-    const Admin = React.lazy(function() {
-        return import("./Admin.js");      // returns promise of import()
-    });
-
-    // Or a shorter way:
-    const Admin = React.lazy(() => import("./Admin.js"));
+    // do NOT lazy load things above the fold like hero images, site logo, etc.
 
 
-// Step 2: wrap with suspense component
-
-    // The Suspense component wraps lazy-loaded components and allows you to render intermediary (loading) content until the component has loaded.
-
-    import React, {Suspense} from "react";
-    import {render} from "react-dom";
-    const Admin = React.lazy(() => import("./Admin.js"));
-
-    function User(props) {
-        return (<>
-            <p>User details show up here</p>
-            {props.isAdmin && <Suspense>
-                <Admin />
-            </Suspense>}
-        </>);
-    }
+// usage:
+    // Admin component (logged in) - bc some people don’t want to log in or are not admins at all
+    // image gallery below the top fold or towards the bottom of page
 
 
-// Step 3: provide intermediary/fallback content 
+    // examples:
 
-    // define what content React will render while the component is loading (while the import() call is going to the network and fetching the file) - provided by a fallback prop to the Suspense component
+        // Step 1: lazy load the imported component
 
-    // Example 1:
-        <Suspense fallback={<div>Loading admin...</div>}>
-            <Admin />
-        </Suspense>
+            // Instead of :
+            Import Admin from ‘./Admin.js’;
 
-
-    // Example 2:
-        <Suspense fallback={<CircleSpinner />}>
-            <Admin />
-        </Suspense>
+            // write :
+            const Admin = React.lazy(() => import("./Admin.js"));
 
 
-// Instead of :
-    import React, {Suspense} from "react";
-    import {render} from "react-dom";
-    import Admin from "./Admin.js";
 
-    function User(props) {
-        return (<>
-            <p>User details show up here</p>
-            {props.isAdmin && <Admin />}
-        </>);
-    }
+        // Step 2: wrap with suspense component
 
-// Write :
-    import React, {Suspense} from "react";
-    import {render} from "react-dom";
-    const Admin = React.lazy(() => import("./Admin.js"));
+            // The Suspense component wraps lazy-loaded components and allows you to render intermediary (loading) content until the component has loaded.
 
-    function User(props) {
-        return (<>
-            <p>User details show up here</p>
-            {props.isAdmin && <Suspense fallback={<div>Loading admin...</div>}>
-                <Admin />
-            </Suspense>}
-        </>);
-    }
+            import React, {Suspense} from "react";
+            import {render} from "react-dom";
+            const Admin = React.lazy(() => import("./Admin.js"));
+
+            function User(props) {
+                return (<>
+                    {props.isAdmin && <Suspense>
+                        <Admin />
+                    </Suspense>}
+                </>);
+            }
+
+
+        // Step 3: provide intermediary/fallback content 
+
+            // define what content React will render while the component is loading (while the import() call is going to the network and fetching the file) - provided by a fallback prop to the Suspense component
+
+            // Example 1:
+                <Suspense fallback={<div>Loading admin...</div>}>
+                    <Admin />
+                </Suspense>
+
+
+            // Example 2:
+                <Suspense fallback={<CircleSpinner />}>
+                    <Admin />
+                </Suspense>
+
+
+
+        // Instead of :
+            import React, {Suspense} from "react";
+            import {render} from "react-dom";
+            import Admin from "./Admin.js";
+
+            function User(props) {
+                return (<>
+                    {props.isAdmin && <Admin />}
+                </>);
+            }
+
+        // Write :
+            import React, {Suspense} from "react";
+            import {render} from "react-dom";
+            const Admin = React.lazy(() => import("./Admin.js"));
+
+            function User(props) {
+                return (<>
+                    {props.isAdmin && <Suspense fallback={<div>Loading admin...</div>}>
+                        <Admin />
+                    </Suspense>}
+                </>);
+            }
 
 
 
@@ -113,5 +113,9 @@
 
     
     
+// *** Do not lazy load every single component. 
+    // This will make your app slower. Generally, try to lazy load components that are very large and that do not show up immediately above the fold.
+
     
-// *** Do not lazy load every single component. This will make your app slower. Generally, try to lazy load components that are very large and that do not show up immediately above the fold.
+    
+    
