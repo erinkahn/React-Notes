d9d156ca736c57e01cb6f1108a0b208b81931dc4
Erin Kahn Smith <erinnicolekahn@gmail.com>
2021-12-28 16:19:27 -0500

    Update apis.js

--- apis.js
+++ apis.js
@@ -1,7 +1,7 @@
 // APIS
 
 
-// examples: 
+// example: 
 
     const addItemToList = (list, item) => {
         return [...list, item];
@@ -16,4 +16,61 @@
 
           item = response.json();
        });
+
+
+// example:
+
+    const fetchData = () => {
+          const stringifyData = data => JSON.stringify(data, null, 2)
+          const initialData = stringifyData({ data: null })
+          const loadingData = stringifyData({ data: 'loading...' })
+          const [data, setData] = useState(initialData)
+
+          const [gender, setGender] = useState('female')
+          const [loading, setLoading] = useState(false)
+
+          useEffect(
+            () => {
+              const fetchData = () => {
+                setLoading(true)
+                const uri = 'https://randomuser.me/api/?gender=' + gender
+                fetch(uri)
+                  .then(res => res.json())
+                  .then(({ results }) => {
+                    setLoading(false)
+                    const { name, gender, dob } = results[0]
+                    const dataVal = stringifyData({
+                      ...name,
+                      gender,
+                      age: dob.age
+                    })
+                    setData(dataVal)
+                  })
+              }
+              fetchData()
+            },
+            [gender]
+          )
+
+          return (
+            <>
+              <button
+                onClick={() => setGender('male')}
+                style={{ outline: gender === 'male' ? '1px solid' : 0 }}
+              >
+                Fetch Male User
+              </button>
+              <button
+                onClick={() => setGender('female')}
+                style={{ outline: gender === 'female' ? '1px solid' : 0 }}
+              >
+                Fetch Female User
+              </button>
+
+              <section>
+                {loading ? <pre>{loadingData}</pre> : <pre>{data}</pre>}
+              </section>
+            </>
+          )
+    }
 
