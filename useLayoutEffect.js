67de73cf8c9f528ae192069d44eecead3ed8c9da
Erin Kahn Smith <erinnicolekahn@gmail.com>
2021-12-28 14:06:00 -0500

    Update useLayoutEffect.js

--- useLayoutEffect.js
+++ useLayoutEffect.js
@@ -1,4 +1,21 @@
 // useLayoutEffect hook:
-  // When measuring the layout of a page
+  // When measuring the layout of a page, visual changes to the DOM
   // It changes every render and replaces the previous render result with a new one
+  // has dependencies [ ]
 
+
+// Difference between useLayoutEffect and useEffect:
+  // useLayoutEffect renders *BEFORE* the browser screen is painted (runs synchronously)
+  // useEffect renders *AFTER* the browser screen is painted (runs asynchronously)
+
+
+// use cases:
+  // when you see flickering, you should use this instead of useEffect
+  // maps, interfaces
+
+
+// syntax:
+
+    useLayoutEffect(() => {
+      //do something
+    }, [arrayDependency])
