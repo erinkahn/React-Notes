b4c2ed3bd67ad7a2a1ac6bca47889ff8b6a5ae0d
Erin Kahn Smith <erinnicolekahn@gmail.com>
2021-12-28 15:38:03 -0500

    Update useMemo.js

--- useMemo.js
+++ useMemo.js
@@ -1,15 +1,20 @@
 // useMemo
+  // used to speed / help performance and decrease the need to re-render
   // returns a memoized (remembered) value without re-rendering over and over
   // different from useCallback, it internalizes return values instead of entire functions
   // Rather than passing a handle to the same function, React skips the function and returns the previous result, until the parameters change.
   // Before the next render, if the new props are the same, React reuses the memoized result skipping the next rendering.
 
 
+// syntax
+  const memoizedResult = useMemo(() => expensiveComputation(a, b), [a, b])
+
+
 // when to use
-  // component renders often
-  // your component is big
-  // when you expect a component to render often and usually with the same props
+  // first write the calculation without useMemo and only add it if the original unmemoized component is causing performance issues
+  // if your component is big, renders often and usually has the same props
   // The more often the component renders with the same props, the heavier and the more computationally expensive the output is, the more chances are that component needs to be wrapped in React.memo().
+
 
 // example
 
@@ -22,6 +27,4 @@
     }
     export const MemoizedMovie = React.memo(Movie); // React.memo(Movie) returns a new memoized component MemoizedMovie.
 
-
-
-
+  
