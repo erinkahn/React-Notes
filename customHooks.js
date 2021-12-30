640d130a70dd10464f67a6d64f9a27f372efc42c
Erin Kahn Smith <erinnicolekahn@gmail.com>
2021-12-30 15:44:32 -0500

    Update customHooks.js

--- customHooks.js
+++ customHooks.js
@@ -3,6 +3,30 @@
   // allow us to write functions once and reuse them whenever they are needed and hence obeying the DRY principle.
 
 
+// create the custom hook, then use it in a component like so:
+    
+  // 1. custom hook code:
+    function useMyCustomHook(someDataKey) {
+
+        const [someValue, setSomeValue] = useState(null);
+
+        useEffect(() => {
+            setSomeValue(useSomeOtherHook(someDataKey));
+        }, [someDataKey]);
+
+        return someNewValue;
+    }
+
+  // 2. consumer component code
+    function MyAwesomeComponent({someDataKey}) {
+
+        const someValue = useMyCustomHook(someDataKey);
+
+        return (<p>The new value is {someValue}</p>);
+    }
+
+
+
 // examples:
 
 
@@ -38,7 +62,6 @@
       };
 
 
-
     // --- then in App.js or your component file call the hook
 
       import { useScrollPos } from "./useScrollPos";
@@ -55,4 +78,47 @@
 
 
 
+  // ---- get data = useAxios()
 
+    import {useState, useEffect} from 'react';
+    import axios from 'axios';
+
+    export default function useAxios(url) {
+        const [data, setData] = useState();
+        const [error, setError] = useState();
+        const loading = !data && !error;
+
+        useEffect(() => {
+            axios.get(url).then((response) => {
+                setData(response.data);
+            }).catch(error => {
+                setError(error);
+                console.log(error, 'oops, the request is not working at the moment')
+            });
+        }, [url]);
+
+        return {
+            data,
+            loading,
+            error
+        }
+    }
+
+
+  // ---- then in your component file...
+
+  import useAxios from "../hooks/useAxios";
+  const API_KEY = 'fe577b382a8038af35c74879570e375e';
+
+  export default function ComponentName() {
+      const url = `https://api.openweathermap.org/data/2.5/onecall?&units=imperial&lat=${position.lat}&lon=${position.long}&exclude=daily&appid=${API_KEY}`;
+      const {data, loading, error} = useAxios(url);
+      if (loading || !data) return '...';
+      if (error) return '';
+
+      return (<>
+          <div className="data-container">
+              {data}
+          </div>
+      </>)
+  }
