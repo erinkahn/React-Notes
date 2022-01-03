eebf6aa53b55d137d5967093311ccd687dc2cccf
Erin Kahn Smith <erinnicolekahn@gmail.com>
2022-01-03 14:32:54 -0500

    Update testing.js

--- testing.js
+++ testing.js
@@ -12,14 +12,21 @@
   // npm install web-vitals
 
   // then, in index.js paste
-    import {getLCP, getFID, getCLS} from 'web-vitals';
+    import {getLCP, getFCP, getFID, getCLS} from 'web-vitals';
 
     getCLS(console.log); // cumulative layout shift (loading)
+    getFCP(console.log); // first contentful paint (loading)
     getFID(console.log); // first input delay (interactivity)
     getLCP(console.log); // largest contentful paint (visual stability)
+    getTTFB(console.log); // time to first byte (redirectconnect/request time)
 
   // then inspect and see the issues going on
 
 
 // Pageseed Insights - https://pagespeed.web.dev/?utm_source=psi&utm_medium=redirect
 // Webpage test - https://webpagetest.org/
+
+
+
+// --- Jest - https://jestjs.io/ 
+// --- react testing library - https://github.com/testing-library/react-testing-library 
