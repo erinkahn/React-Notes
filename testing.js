ca28c4021079b222aa5fb0ac186f72af5482ffc6
Erin Kahn Smith <erinnicolekahn@gmail.com>
2022-01-03 14:58:14 -0500

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,9 +1,19 @@
 // Performance testing a React app
 
-// BASICS:
-  // LCP (largest contentful paint) should be 2.5 seconds or less
-  // FID (first input delay) event should be 100 milliseconds or less
-  // CLS (cumulative layout shift) should be 0.1 or less
+// Considerations:
+  // 1. Perceived load speed: how quickly a page can load and render all of its visual elements to the screen.
+  // 2. Load responsiveness: how quickly a page can load and execute any required JavaScript code in order for components to respond quickly to user interaction
+  // 3. Runtime responsiveness: after page load, how quickly can the page respond to user interaction.
+  // 4. Visual stability: do elements on the page shift in ways that users don't expect and potentially interfere with their interactions?
+  // 5. Smoothness: do transitions and animations render at a consistent frame rate and flow fluidly from one state to the next
+
+// Metrics to measure:
+  // FCP (first contentful paint) - https://web.dev/fcp/
+  // LCP (largest contentful paint) should be 2.5 seconds or less - https://web.dev/lcp/
+  // FID (first input delay) event should be 100 milliseconds or less - https://web.dev/fid/
+  // CLS (cumulative layout shift) should be 0.1 or less - https://web.dev/cls/
+  // TTI (time to interactive) - https://web.dev/tti/
+  // TBT (total blocking time) - https://web.dev/tbt/
 
 // --- Web Vitals
 
