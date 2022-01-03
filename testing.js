eabdb9efb2293a95bd0dc5a9327294502ac804aa
Erin Kahn Smith <erinnicolekahn@gmail.com>
2022-01-03 13:40:26 -0500

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,2 +1,31 @@
 // Performance testing a React app
 
+// Web Vitals
+  // check if web-vitals is installed in package.json
+  // if not, install it 
+  // npm install web-vitals
+
+  // then, create a file called WebVitals.js and paste this code
+  
+    import { useEffect } from "react";
+    import { getCLS, getFID, getLCP, getTTFB } from "web-vitals";
+
+    const WebVitals = () => {
+      useEffect(() => {
+        getTTFB(console.log);
+        getCLS(console.log);
+        getFID(console.log);
+        getLCP(console.log);
+      }, []);
+
+      return null;
+    };
+
+    export default WebVitals;
+
+  // inside App.js import the WebVitals component like so:
+
+    import WebVitals from './test/WebVitals'; // for testing
+    
+  // and render it inside your app component
+    <WebVitals/>
