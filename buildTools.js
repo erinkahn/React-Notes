f8455d2e688c7bba7fc6b4b6e777ca549bc8d354
Erin Kahn Smith <erinnicolekahn@gmail.com>
2022-01-06 14:48:27 -0500

    Create buildTools.js

---
+++ buildTools.js
@@ -0,0 +1,16 @@
+// Build Tools
+  // 1. package managers  - npm, yarn, homebrew
+  // 2. bundlers          - webpack, browserify, parcel, popup, vite, rollup
+  // 3. task runners      - gulp, grunt
+
+
+// what do build tools do?
+  // 1. minify code for production which will improve performance
+  // 2. run unit tests with one command
+  // 3. preview app changes automatically 
+
+
+// web development ecosystem:
+  // 1. development - HTML, CSS, JS, REACT, NODE
+  // 2. testing - JEST, Mocha
+  // 3. deployment - task runners and bundlers
