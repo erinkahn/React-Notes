e393b2fe0e0c0f23e995a4a717fd226c3779cc74
Erin Kahn Smith <erinnicolekahn@gmail.com>
2022-01-11 16:50:35 -0500

    Update testing_RTL.js

--- testing_RTL.js
+++ testing_RTL.js
@@ -2,3 +2,14 @@
     // UI-layer testing framework to make sure components are rendering and working properly
     // https://testing-library.com/docs/react-testing-library/intro/
 
+    // installation
+        npm install @testing-library/react --save-dev
+
+    // import 2 values into tests
+       import { render, screen } from '@testing-library/react'
+
+    // create a test
+        test('should say hello') () => {
+            render(<Greeting/>);
+            screen.debug();
+        })
