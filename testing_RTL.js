1ceaf82141f7367d421700f1a60952ede37e4ae4
Erin Kahn Smith <erinnicolekahn@gmail.com>
2022-01-11 16:42:55 -0500

    Create testing_RTL.js

---
+++ testing_RTL.js
@@ -0,0 +1,4 @@
+// RTL = REACT TESTING LIBRARY  
+    // UI-layer testing framework to make sure components are rendering and working properly
+    // https://testing-library.com/docs/react-testing-library/intro/
+
