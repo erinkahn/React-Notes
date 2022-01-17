7c81e0766125c0090d809a00a5f2e8a614feaa3a
Erin Kahn Smith <erinnicolekahn@gmail.com>
2022-01-17 16:03:48 -0500

    Update fullpage_nav_modal.js

--- fullpage_nav_modal.js
+++ fullpage_nav_modal.js
@@ -32,72 +32,72 @@ const Navigation = () => {
 
 	return (
 		<>
-			<button
-				className={`menuBtn ${hidden ? "" : "open"}`}
-				aria-label={`${hidden ? "Open Navigation" : "Close Navigation"}`}
-				onClick={toggle}
-				ref={btnRef}
-				tabIndex="0"
-			>
-				{hidden ? "Menu" : "Close"}
-			</button>
+		<button
+			className={`menuBtn ${hidden ? "" : "open"}`}
+			aria-label={`${hidden ? "Open Navigation" : "Close Navigation"}`}
+			onClick={toggle}
+			ref={btnRef}
+			tabIndex="0"
+		>
+			{hidden ? "Menu" : "Close"}
+		</button>
 
-			<div
-				className={`fullPageMenu ${hidden ? "" : "appear"}`}
-				aria-hidden={`${hidden ? "true" : "false"}`}
-				role="dialog"
-				aria-modal="true"
-				aria-labelledby="full page menu"
-				onKeyUp={escapeModal}
-				onBlur={handleFocus}
-			>
-				<div className="nav-wrapper">
-					<ul className="social">
-						{navData.socialIcons.map((icon) => (
-							<li
-								className="icon-list-item"
-								key={`icon-${icon.id}`}
+		<div
+			className={`fullPageMenu ${hidden ? "" : "appear"}`}
+			aria-hidden={`${hidden ? "true" : "false"}`}
+			role="dialog"
+			aria-modal="true"
+			aria-labelledby="full page menu"
+			onKeyUp={escapeModal}
+			onBlur={handleFocus}
+		>
+			<div className="nav-wrapper">
+				<ul className="social">
+					{navData.socialIcons.map((icon) => (
+						<li
+							className="icon-list-item"
+							key={`icon-${icon.id}`}
+						>
+							<a
+								className={`icon ${icon.siteName}`}
+								target="_blank"
+								rel="noreferrer"
+								href={icon.url}
 							>
-								<a
-									className={`icon ${icon.siteName}`}
-									target="_blank"
-									rel="noreferrer"
-									href={icon.url}
+								<span className="sr-only">
+									{icon.siteName}
+								</span>
+							</a>
+						</li>
+					))}
+				</ul>
+
+				<p className="copyright">
+					Copyright &copy; {today}. All rights reserved.
+				</p>
+
+				<nav>
+					<ul className="nav-list">
+						{navData.navigation.map((item) => (
+							<li
+								key={`nav-${item.id}`}
+								className={`nav-item ${
+									hidden ? "" : "open"
+								}`}
+							>
+								<NavLink
+									className="nav-link"
+									to={item.linkTo}
 								>
-									<span className="sr-only">
-										{icon.siteName}
-									</span>
-								</a>
+									{item.pageTitle}
+									<span className="sr-only">Page</span>
+								</NavLink>
 							</li>
 						))}
 					</ul>
-
-					<p className="copyright">
-						Copyright &copy; {today}. All rights reserved.
-					</p>
-
-					<nav>
-						<ul className="nav-list">
-							{navData.navigation.map((item) => (
-								<li
-									key={`nav-${item.id}`}
-									className={`nav-item ${
-										hidden ? "" : "open"
-									}`}
-								>
-									<NavLink
-										className="nav-link"
-										to={item.linkTo}
-									>
-										{item.pageTitle}
-										<span className="sr-only">Page</span>
-									</NavLink>
-								</li>
-							))}
-						</ul>
-					</nav>
-				</div>
+				</nav>
 			</div>
+		</div>
 		</>
 	);
 };
