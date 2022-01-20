68f4e4d4b22847af8e841ad9857645e75ba9d657
Erin Kahn Smith <erinnicolekahn@gmail.com>
2022-01-20 15:06:17 -0500

    Update lifecycle.js

--- lifecycle.js
+++ lifecycle.js
@@ -6,13 +6,35 @@
 
 // Component Lifecycle
   // the sequence of stages an instance of a component goes through in the DOM
+  // has 3 categories:
+      // 1. mounting
+      // 2. updating
+      // 3. unmounting
 
 
-// Lifecycle methods are ONLY used in class components
-  // 1. componentDidMount()     - component is BORN & inserted in the DOM tree (things happening: APIs, setTimeout, Intervals, event listeners)
-  // 2. render()                - component is shown (required in all class components) 
+// TOP 4 Lifecycle methods 
+  // 1. render()                - component is shown (required in all class components) * you CANNOT update state within render()
+  // 2. componentDidMount()     - component is BORN & inserted in the DOM tree (things happening: APIs, setTimeout, Intervals, event listeners)
   // 3. componentDidUpdate()    - component is updated (props are changing/updating)
-  // 4. componentWillUnmount()  - component is DEAD (things happening: remove event listeners, cancel network requests, cleanup functions)
+  // 4. componentWillUnmount()  - component is DESTROYED (things happening: remove event listeners, cancel network requests, cleanup functions) * you CANNOT update state within this method
+
+
+* // lifecycle methods are ONLY used in class components
+* // you can only update or modify a components state inside the componentDidUpdate() method or the componentDidMount() method (be cautious updating state in the mount method)
+* // you will need to wrap setState() in a condition to check for state or prop changes from previous state, because without it, it can lead to an infinite loop.
+
+    // example:
+     componentDidUpdate(prevProps) {
+       if (this.props.userName !== prevProps.userName) { //don't forget to compare the props
+         this.fetchData(this.props.userName); // wrapped in conditional
+       }
+      }
+
+
+// other lifecycle methods: (USE SPARINGLY / NOT OFTEN - can cause performance issues)
+  // 5. shouldComponentUpdate()   - returns boolean, if you need to tell React not to re-render for a certain state or prop change
+  // 6. static getDerivedStateFromProps(props, state) - NEW & safer than componentWillReceiveProps(), it renders on every fire, is called before render()
+  // 7. getSnapshotBeforeUpdate()  - NEW & safer than componentWillUpdate(), called b4 DOM updates
 
 
 // Mounting 
