e102e2adb03d566cf8fdb1bdeb0cc72be2927982
Erin Kahn Smith <erinnicolekahn@gmail.com>
2022-01-31 10:09:48 -0500

    Create typescript.ts

---
+++ typescript.ts
@@ -0,0 +1,15 @@
+// typescript
+  // a superset of JS
+  // built by Microsoft in 2012
+  // a programming language similar to JS but with new features and a type system
+    // type system - shows bugs, clarifies structure, refactors code
+  // file type is .ts
+
+
+  // code is transpiled into JS that checks if code has correct standards and shows errors if it doesnt
+  // transpiles and outputs a JS file
+
+
+// why typescript instead of JS?
+  // best for larger applications especially for run time
+
