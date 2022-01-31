c5d304514722e1b6a816a0c63193a6c1caf6e4f0
Erin Kahn Smith <erinnicolekahn@gmail.com>
2022-01-31 10:11:52 -0500

    Update typescript.ts

--- typescript.ts
+++ typescript.ts
@@ -2,12 +2,17 @@
   // a superset of JS
   // built by Microsoft in 2012
   // a programming language similar to JS but with new features and a type system
-    // type system - shows bugs, clarifies structure, refactors code
   // file type is .ts
+  
 
+// type system - shows bugs, clarifies structure, refactors code
+  
 
+// HOW IS CODE TRANSPILED ? 
   // code is transpiled into JS that checks if code has correct standards and shows errors if it doesnt
   // transpiles and outputs a JS file
+  // compile with command:
+    tsc
 
 
 // why typescript instead of JS?
