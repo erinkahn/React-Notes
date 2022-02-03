1803ad76530aec84be0b208f856ae16b717c4368
Erin Kahn Smith <erinnicolekahn@gmail.com>
2022-02-03 16:16:48 -0500

    Update typescript.ts

--- typescript.ts
+++ typescript.ts
@@ -303,9 +303,19 @@
       
       
       
+// tuple - an array with specific type elements
       
+      let favoriteCoordinates: [number, number, string, number, number, string] = [40, 43.2, 'N', 73, 59.8, 'W'];
       
+      let tup: [string, string] = ['hi', 'bye'];
+      let arr: string[] = ['there','there'];
+        tup = ['there', 'there']; // No Errors.
+        tup = arr; // Type Error! An array cannot be assigned to a tuple.
       
+      // examples + errors
+      let ourTuple: [string, number, string, boolean] = ['Is', 7 , 'our favorite number?' , false]; // correct
+      let numbersTuple: [number, number, number] = [1,2,3,4]; // Type Error! numbersTuple should only have three elements
+      let mixedTuple: [number, string, boolean] = ['hi', 3, true] // Type Error! The first elements should be a number, the second a string, and the third a boolean. 
       
       
       
