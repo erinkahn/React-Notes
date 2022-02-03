fc6d6d1ccb9f8ea6b909210ba2f6cb52c0bac2db
Erin Kahn Smith <erinnicolekahn@gmail.com>
2022-02-03 15:48:09 -0500

    Update typescript.ts

--- typescript.ts
+++ typescript.ts
@@ -276,7 +276,11 @@
   let names: string[] = [1,2,3]; // Type Error
   names.push(566) // Type Error
       
-      
+    // examples
+      let bestNumbers: number[] = [7,77,4];
+      let bestLunches: string[] = ['chicken soup', 'non-chicken soup'];
+      let bestBreakfasts: string[] = ['fasting', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
+      let bestBooleans: boolean[] = [true, false];
       
       
       
